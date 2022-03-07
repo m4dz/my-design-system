3997b298f013b096c7eea157478ac9bcb3ca7f9a
m4dz <471486+undefined@users.noreply.github.com>
2022-03-07 14:56:22 +0000

    Push from my-button

---
+++ MyButton/stories/index.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '~/MyButton';
+
+export default {
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const myButtonStory = () => <Button />;
